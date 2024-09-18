0b456c10beccc22769d1b586997bd9608d07cd60
epainos <epainos@knou.ac.kr>
2024-09-18 21:11:41 +0900

    modified:   app/spawn_test.go

--- app/spawn_test.go
+++ app/spawn_test.go
@@ -4,8 +4,18 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"runtime"
+	"strings"
 	"testing"
 )
+
+// ifElse 함수 정의
+func ifElse(condition bool, trueVal string, falseVal string) string {
+	if condition {
+		return trueVal
+	}
+	return falseVal
+}
 
 func TestExpandMacro(t *testing.T) {
 	g := NewGoful("")
@@ -16,37 +26,37 @@ func TestExpandMacro(t *testing.T) {
 		in  string
 		out string
 	}{
-		{`%f`, `".."`},
-		{`%F`, fmt.Sprintf(`"%s"`, filepath.Dir(home))},
-		{`%x`, `"."`},
-		{`%X`, fmt.Sprintf(`"%s"`, filepath.Dir(home))},
-		{`%m`, `".."`},
-		{`%M`, fmt.Sprintf(`"%s"`, filepath.Dir(home))},
-		{`%d`, fmt.Sprintf(`"%s"`, filepath.Base(home))},
-		{`%D`, fmt.Sprintf(`"%s"`, home)},
-		{`%d2`, fmt.Sprintf(`"%s"`, filepath.Base(home))},
-		{`%D2`, fmt.Sprintf(`"%s"`, home)},
-		{`%~f`, ".."},
-		{`%~F`, filepath.Dir(home)},
-		{`%~x`, "."},
-		{`%~X`, filepath.Dir(home)},
+		{`%f`, `'..'`},
+		{`%F`, fmt.Sprintf(`'%s'`, ifElse(runtime.GOOS == "windows", strings.Replace(strings.Replace(filepath.Dir(home), `\`, `/`, -1), `"`, `'`, -1), filepath.Dir(home)))},
+		{`%x`, `'.'`},
+		{`%X`, fmt.Sprintf(`'%s'`, ifElse(runtime.GOOS == "windows", strings.Replace(strings.Replace(filepath.Dir(home), `\`, `/`, -1), `"`, `'`, -1), filepath.Dir(home)))},
+		{`%m`, `'..'`},
+		{`%M`, fmt.Sprintf(`'%s'`, ifElse(runtime.GOOS == "windows", strings.Replace(strings.Replace(filepath.Dir(home), `\`, `/`, -1), `"`, `'`, -1), filepath.Dir(home)))},
+		{`%d`, fmt.Sprintf(`'%s'`, ifElse(runtime.GOOS == "windows", strings.Replace(strings.Replace(filepath.Dir(home), `\`, `/`, -1), `"`, `'`, -1), filepath.Base(home)))},
+		{`%D`, fmt.Sprintf(`'%s'`, ifElse(runtime.GOOS == "windows", strings.Replace(strings.Replace(home, `\`, `/`, -1), `"`, `'`, -1), home))},
+		{`%d2`, fmt.Sprintf(`'%s'`, ifElse(runtime.GOOS == "windows", strings.Replace(strings.Replace(filepath.Dir(home), `\`, `/`, -1), `"`, `'`, -1), filepath.Base(home)))},
+		{`%D2`, fmt.Sprintf(`'%s'`, ifElse(runtime.GOOS == "windows", strings.Replace(strings.Replace(home, `\`, `/`, -1), `"`, `'`, -1), home))},
+		{`%~f`, `..`},
+		{`%~F`, ifElse(runtime.GOOS == "windows", strings.Replace(strings.Replace(filepath.Dir(home), `\`, `/`, -1), `"`, `'`, -1), filepath.Dir(home))},
+		{`%~x`, `.`},
+		{`%~X`, ifElse(runtime.GOOS == "windows", strings.Replace(strings.Replace(filepath.Dir(home), `\`, `/`, -1), `"`, `'`, -1), filepath.Dir(home))},
 		{`%~m`, ".."},
-		{`%~M`, filepath.Dir(home)},
-		{`%~d`, filepath.Base(home)},
-		{`%~D`, home},
-		{`%~d2`, filepath.Base(home)},
-		{`%~D2`, home},
-		{`%%%f`, `%%".."`},
+		{`%~M`, ifElse(runtime.GOOS == "windows", strings.Replace(strings.Replace(filepath.Dir(home), `\`, `/`, -1), `"`, `'`, -1), filepath.Dir(home))},
+		{`%~d`, ifElse(runtime.GOOS == "windows", strings.Replace(strings.Replace(filepath.Dir(home), `\`, `/`, -1), `"`, `'`, -1), filepath.Base(home))},
+		{`%~D`, ifElse(runtime.GOOS == "windows", strings.Replace(strings.Replace(home, `\`, `/`, -1), `"`, `'`, -1), home)},
+		{`%~d2`, ifElse(runtime.GOOS == "windows", strings.Replace(strings.Replace(filepath.Dir(home), `\`, `/`, -1), `"`, `'`, -1), filepath.Base(home))},
+		{`%~D2`, ifElse(runtime.GOOS == "windows", strings.Replace(strings.Replace(home, `\`, `/`, -1), `"`, `'`, -1), home)},
+		{`%%%f`, `%%'..'`},
 		{`%%%~f`, `%%..`},
 		{`%~~f`, `%~~f`},
 		{`\%f%f`, `%f".."`},
 		{`\%~f%~f`, `%~f..`},
-		{`%\f%f`, `%f".."`},
+		{`%\f%f`, `%f'..'`},
 		{`%\~f%~f`, `%~f..`},
-		{"%AA%ff", `%AA".."f`},
+		{"%AA%ff", `%AA'..'f`},
 		{"%~A~A%~ff", `%~A~A..f`},
-		{"%m %f", `".." ".."`},
-		{"%~f %f %~m", `.. ".." ..`},
+		{"%m %f", `'..' '..'`},
+		{"%~f %f %~m", `.. '..' ..`},
 	}
 
 	for _, macro := range macros {
